06983010f4d72588e2b588d46c4a0c96115bd06b
Miles Kwok <w.winsoncheng@gmail.com>
2022-07-12 17:49:09 +0800

    finish simpleStorage contract with hardhat

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,51 +1,51 @@
-// imports
-const { ethers, run, network } = require("hardhat")
+const { ethers, run, network } = require("hardhat");
+require("dotenv").config();
 
-// async main
 async function main() {
-  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
-  console.log("Deploying contract...")
-  const simpleStorage = await SimpleStorageFactory.deploy()
-  await simpleStorage.deployed()
-  console.log(`Deployed contract to: ${simpleStorage.address}`)
-  // what happens when we deploy to our hardhat network?
-  if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
-    console.log("Waiting for block confirmations...")
-    await simpleStorage.deployTransaction.wait(6)
-    await verify(simpleStorage.address, [])
-  }
-
-  const currentValue = await simpleStorage.retrieve()
-  console.log(`Current Value is: ${currentValue}`)
-
-  // Update the current value
-  const transactionResponse = await simpleStorage.store(7)
-  await transactionResponse.wait(1)
-  const updatedValue = await simpleStorage.retrieve()
-  console.log(`Updated Value is: ${updatedValue}`)
-}
-
-// async function verify(contractAddress, args) {
-const verify = async (contractAddress, args) => {
-  console.log("Verifying contract...")
-  try {
-    await run("verify:verify", {
-      address: contractAddress,
-      constructorArguments: args,
-    })
-  } catch (e) {
-    if (e.message.toLowerCase().includes("already verified")) {
-      console.log("Already Verified!")
-    } else {
-      console.log(e)
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    );
+    console.log("Deploying contract...");
+    const simpleStorage = await SimpleStorageFactory.deploy();
+    await simpleStorage.deployed();
+    //what's the private key? what's the rpc url?
+    //hardhat generates it during the deployment (remember the 10 fake accounts?)
+    console.log(`Deployed contract to: ${simpleStorage.address}`);
+    if (network.config.chainId == 4 && process.env.ETHERSCAN_API_KEY) {
+        console.log("Waiting for block txes...");
+        await simpleStorage.deployTransaction.wait(6);
+        await verify(simpleStorage.address, []);
     }
-  }
+
+    const currentValue = await simpleStorage.retrieve();
+    console.log(`Current Value is ${currentValue}`);
+
+    //Update the current Value
+    const transactionResponse = await simpleStorage.store(7);
+    await transactionResponse.wait(1);
+    const updatedValue = await simpleStorage.retrieve();
+    console.log(`Updated Value is ${updatedValue}`);
 }
 
-// main
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
+// async function verify(contractAddress, args){
+const verify = async (contractAddress, args) => {
+    console.log("Verifying Contract...");
+
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        });
+    } catch (error) {
+        if (error.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!");
+        } else {
+            console.log(error);
+        }
+    }
+};
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
